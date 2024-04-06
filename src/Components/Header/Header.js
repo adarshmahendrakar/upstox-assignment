ca869c3654b2eb4d5c0a085c66cb8d7f87e12090
adarsh.mahendrakar <you@example.com>
2024-04-07 02:37:58 +0530

    header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {HeaderContainer, Title} from './HeaderStyles';
+
+const Header = ({title}) => {
+  return (
+    <HeaderContainer>
+      <Title>{title}</Title>
+    </HeaderContainer>
+  );
+};
+
+export default Header;

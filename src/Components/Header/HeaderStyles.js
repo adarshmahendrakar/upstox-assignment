bf84a71816e40a08ea849e98f8534f34425e8aa0
adarsh.mahendrakar <you@example.com>
2024-04-07 02:38:21 +0530

    header

---
+++ src/Components/Header/HeaderStyles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+export const HeaderContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 15px;
+  background-color: #9400d3;
+`;
+
+export const Title = styled.Text`
+  color: #fff;
+  font-size: 20px;
+  font-weight: bold;
+`;
